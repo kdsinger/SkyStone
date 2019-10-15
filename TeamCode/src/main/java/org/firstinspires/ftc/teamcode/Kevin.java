0f4091954fb58f5cd47da24b23207d96f116d54c
Kevin Singer <kdsinger@gmail.com>
2019-10-15 08:02:04 -0700

    First commit for demo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kevin.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Kevin implements Gamepad.GamepadCallback {
+    /**
+     * This method will be called whenever the gamepad state has changed due to either a KeyEvent
+     * or a MotionEvent.
+     *
+     * @param gamepad device which state has changed
+     */
+    @Override
+    public void gamepadChanged(Gamepad gamepad) {
+
+    }
+}
